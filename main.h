a474ac0de6311acf33af5a21bcb3c7c5c13072dd
issar13 <issa_suleiman008@hotmail.com>
2022-04-15 19:08:19 +0300

    trial

--- main.h
+++ main.h
@@ -1,4 +1,23 @@
-#ifndef MAINN_H
+#ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+
+int _putchar(char a);
 int _printf(const char *format, ...);
-#endif /* MAIN_H */
+
+
+#endif /* MAIN_H *
