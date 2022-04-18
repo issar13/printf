ee8516bac73e56955c91721f2cb4e40db23e6770
issar13 <issa_suleiman008@hotmail.com>
2022-04-18 17:50:29 +0300

    initial play

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+
+int _putchar(char a);
+int _printf(const char *format, ...);
+int op_c(va_list op_l);
+int op_s(va_list op_l);
+int op_mod(va_list op_l);
+int op_d(va_list op_l);
+
+#endif /* HOLBERTON_H */
---
+++ printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - Entry Point
+ * Desc: _printf function
+ * @format: format type
+ * Return: Function that produces output according to a format.
+ */
+int _printf(const char *format, ...)
+{
+	op_t ops[] = {{"c", op_c}, {"s", op_s}, {"%", op_mod},
+		      {"d", op_d}, {"i", op_d}, {NULL, NULL}};
+	va_list op_l;
+	int i = 0, j;
+	int cont = 0, bandera;
+
+	if (format == NULL || (format[i] == '%' && format[(i + 1)] == '\0'))
+		return (-1);
+	va_start(op_l, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			bandera = 0;
+			for (j = 0; ops[j].op != NULL; j++)
+			{
+				if (format[i + 1] == ops[j].op[0])
+				{
+					cont += ops[j].f(op_l);
+					bandera++;
+				}
+			}
+			if (bandera > 0)
+				i++;
+			else
+			{
+				_putchar(format[i]);
+				cont++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			cont++;
+		}
+	}
+	va_end(op_l);
+	return (cont);
+}
